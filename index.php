7f710030aa222ef8095238fffe4a253a0ccc6170
DarrylBD99 <darrylhoBD@gmail.com>
2024-12-25 14:49:57 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>HTML 5 Boilerplate</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>This website is the main webpage for DatBoi Studio</h1>
+        <h2>Please contact us via email on support@datboistudio.com</h2>
+    </body>
+    <footer>
+        Copyright © 2024, DatBoi Studio
+    </footer>
+</html>
